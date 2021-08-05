74d67ee7268f4b0d3f2fa702e37fd6762f42eebb
Danish Virani <danishvirani.tech@gmail.com>
2021-08-05 15:47:31 -0400

    started on front-end work

--- blog-front-end/src/App.js
+++ blog-front-end/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => }
+
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newTitle, setNewTitle] = useState('')
+  const [newPost, setNewPost] = useState('')
+  const [newFeeling, setNewFeeling] = useState('')
+  const [newImage, setNewImage] = useState('')
+  const [newComments, setNewComments] = useState('')
+
+  const handleNewAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleNewTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleNewPostChange = (event) => {
+    setNewPost(event.target.value)
+  }
+
+  const handleNewFeelingChange = (event) => {
+    setNewFeeling(event.target.value)
+  }
+
+  const handleNewCommentChange = (event) => {
+    setNewComments(event.target.value)
+  }
+
+  const handleNewImageChange = (event) => {
+    setNewImage(event.target.value)
+  }
+
+  const handleNewPostFormSubmit = (event) => {
+    event.preventDefault()
+    axios.post(
+      'http://localhost:3000/posts',
+      {
+        author:newAuthor,
+        post:newPost,
+        title:newTitle,
+        image:newImage,
+        feeling:newFeeling,
+        comments:newComments
+      }
+    ).then(() => {
+      axios
+        .get('http://localhost:3000/animals')
+        .then((response) => {
+          setAnimals(response.data)
+        })
+    })
+  }
+
+
+  return ()
+
 }
 
 export default App;
